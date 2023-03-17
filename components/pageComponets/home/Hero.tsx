46057d32c9eec8b23e566d0b4718b436003508d4
codesleyah <93673396+codesleyah@users.noreply.github.com>
2023-03-17 11:42:02 +0200

    video background

--- components/pageComponets/home/Hero.tsx
+++ components/pageComponets/home/Hero.tsx
@@ -1,7 +1,16 @@
 const Hero = () => {
   return (
-    <div className="h-screen w-screen bg-hero bg-cover flex flex-col md:grid md:grid-cols-2 items-center justify-center px-8 md:px-64">
-      <div className="items-center justify-center">
+    <div className="h-screen w-screen  bg-cover flex flex-col md:grid md:grid-cols-2 items-center justify-center px-8 md:px-64 relative">
+      <video
+        autoPlay
+        loop
+        muted
+        className="absolute w-screen h-screen top-0 object-cover"
+      >
+        <source src="videos/backg.mp4" />
+      </video>
+      <div className="absolute bg-transparent-white w-screen h-screen top-0"></div>
+      <div className="items-center justify-center z-10">
         <h1 className="text-white text-4xl md:text-8xl font-bold">
           SUSTAINABILITY, QUALITY & INNOVATION
         </h1>
