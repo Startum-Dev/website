0f02dc465b74c295385c3792674087bfe68291c3
codesleyah <93673396+codesleyah@users.noreply.github.com>
2023-03-19 05:03:40 +0200

    mobile navbar

--- components/dropdowns/MobileDropDownMenu.tsx
+++ components/dropdowns/MobileDropDownMenu.tsx
@@ -1,44 +1,57 @@
 import React from "react";
 import * as DropdownMenu from "@radix-ui/react-dropdown-menu";
-import { BsWhatsapp, BsInstagram, BsTwitter, BsMenuUp } from "react-icons/bs";
-import ButtonBlue from "../buttons/ButtonBlue";
+import { GiHamburgerMenu } from "react-icons/gi";
+import ServicesDropDownMenu from "./ServicesDropDown";
 import Link from "next/link";
 
 export default function MobileDropDownMenu() {
   return (
     <DropdownMenu.Root>
       <DropdownMenu.Trigger>
-        <BsMenuUp className="text-black" />
+        <GiHamburgerMenu className="text-text-color" />
       </DropdownMenu.Trigger>
       <DropdownMenu.Portal>
         <DropdownMenu.Content
           sideOffset={5}
-          className="bg-white rounded shadow p-2 text-primary-blue"
+          className="bg-primary-black  rounded shadow p-2 text-primary-blue"
         >
           <DropdownMenu.Item className="text-sm font-semibold flex gap-2 items-center justify-start p-2 "></DropdownMenu.Item>
           <DropdownMenu.Item className="text-sm font-semibold flex gap-2 items-center justify-start p-2 ">
             <Link href="/">
-              <text className="text-black text-lg font-semibold">Home</text>
+              <text className="text-white text-lg font-semibold hover:text-primary-yellow">
+                HOME
+              </text>
             </Link>
+          </DropdownMenu.Item>
+          <DropdownMenu.Item className="text-sm font-semibold flex gap-2 items-center justify-start p-2 ">
+            <ServicesDropDownMenu />
           </DropdownMenu.Item>
           <DropdownMenu.Item className="text-sm font-semibold flex gap-2 items-center justify-start p-2">
             <Link href="/blog">
-              <text className="text-black text-lg font-semibold">Blog</text>
+              <text className="text-white text-lg font-semibold hover:text-primary-yellow">
+                BLOG
+              </text>
             </Link>
           </DropdownMenu.Item>
           <DropdownMenu.Item className="text-sm font-semibold flex gap-2 items-center justify-start p-2">
             <Link href="/about-us">
-              <text className="text-black text-lg font-semibold">About</text>
+              <text className="text-white text-lg font-semibold hover:text-primary-yellow">
+                ABOUT
+              </text>
             </Link>
           </DropdownMenu.Item>
           <DropdownMenu.Item className="text-sm font-semibold flex gap-2 items-center justify-start p-2">
             <Link href="/contact-us">
-              <text className="text-black text-lg font-semibold">Contact</text>
+              <text className="text-white text-lg font-semibold hover:text-primary-yellow">
+                CONTACT
+              </text>
             </Link>
           </DropdownMenu.Item>
           <DropdownMenu.Item className="items-center justify-center flex">
-            <Link href="/#services">
-              <ButtonBlue text="Our Services" onClick={() => {}} />
+            <Link href="#d">
+              <button className="rounded border-2 border-white text-white hover:text-primary-yellow hover:border-primary-yellow transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 text-sm font-bold p-2">
+                PROJECT HUB
+              </button>
             </Link>
           </DropdownMenu.Item>
         </DropdownMenu.Content>
