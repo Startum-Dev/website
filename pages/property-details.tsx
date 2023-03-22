76bc8eb1e44e81e733296f6972df5bec4c8492be
codesleyah <93673396+codesleyah@users.noreply.github.com>
2023-03-23 00:05:02 +0200

    removed details page

--- pages/property-details.tsx
+++ pages/property-details.tsx
@@ -1,12 +1,7 @@
 import AppLayout from "../components/layouts/AppLayout";
-import PropertyDetails from "../components/pageComponets/services/realEastate/PropertyDetails";
 
 const RealEstate = () => {
-  return (
-    <AppLayout>
-      <PropertyDetails />
-    </AppLayout>
-  );
+  return <AppLayout></AppLayout>;
 };
 
 export default RealEstate;
