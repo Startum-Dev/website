0a15b9411a7d596dbc6928729390dfb6e050e147
codesleyah <93673396+codesleyah@users.noreply.github.com>
2023-03-23 16:23:24 +0200

    replaced the backg color

--- components/layouts/AppLayout.tsx
+++ components/layouts/AppLayout.tsx
@@ -8,7 +8,7 @@ type LayoutProps = {
 
 const AppLayout = ({ children }: LayoutProps) => {
   return (
-    <div className="px-4 md:px-64 items-center justify-center gap-8 flex flex-col bg-white font-sans bg-background-black text-white">
+    <div className="px-4 md:px-64 items-center justify-center gap-8 flex flex-col font-sans bg-background-black text-white">
       <TransparentNavbar />
       {children}
       <Footer />
