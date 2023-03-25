0b47df18505c5922103bb3e686d545293de44fa9
codesleyah <93673396+codesleyah@users.noreply.github.com>
2023-03-25 02:24:19 +0200

    solar tab

--- components/pageComponets/services/enerygy/solarTab.tsx
+++ components/pageComponets/services/enerygy/solarTab.tsx
@@ -7,16 +7,16 @@ const SolarTab = () => {
       <h1 className="text-3xl md:text-6xl font-semibold text-text-color">
         Solar
       </h1>
-      <div className="w-full grid grid-cols-3 gap-4">
+      <div className="w-full flex-col md:grid md:grid-cols-3 gap-4">
         <SolarCard image="/images/sol1.jpg" heading="Technical Services" />
-        <SolarCard image="/images/sol1.jpg" heading="Technical Services" />
-        <SolarCard image="/images/sol1.jpg" heading="Technical Services" />
-        <SolarCard image="/images/sol1.jpg" heading="Technical Services" />
-        <SolarCard image="/images/sol1.jpg" heading="Technical Services" />
-        <SolarCard image="/images/sol1.jpg" heading="Technical Services" />
-        <SolarCard image="/images/sol1.jpg" heading="Technical Services" />
-        <SolarCard image="/images/sol1.jpg" heading="Technical Services" />
-        <SolarCard image="/images/sol1.jpg" heading="Technical Services" />
+        <SolarCard image="/images/sol2.jpg" heading="Energy panels" />
+        <SolarCard image="/images/sol3.jpg" heading="Wind Generators" />
+        <SolarCard image="/images/sol4.jpg" heading="Solar PV Systems" />
+        <SolarCard image="/images/sol5.jpg" heading="Inspection Checklist" />
+        <SolarCard image="/images/sol6.jpg" heading="Solar Technology" />
+        <SolarCard image="/images/sol9.jpg" heading="Energy Manufacture" />
+        <SolarCard image="/images/sol8.jpg" heading="Solar Equipment" />
+        <SolarCard image="/images/sol7.jpg" heading="Solar Technology" />
       </div>
     </div>
   );
