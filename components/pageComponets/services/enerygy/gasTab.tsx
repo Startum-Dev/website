d5b65f6b9becb0a496ef2a02ac487ddbb4db027e
codesleyah <93673396+codesleyah@users.noreply.github.com>
2023-03-28 01:48:21 +0200

    added the gas page

--- components/pageComponets/services/enerygy/gasTab.tsx
+++ components/pageComponets/services/enerygy/gasTab.tsx
@@ -3,7 +3,9 @@ import Image from "next/image";
 const GasTab = () => {
   return (
     <div className="w-full h-full flex flex-col items-center justify-center gap-8">
-      <h1 className="text-black text-3xl md:text-6xl font-bold">Gas</h1>
+      <h1 className="text-3xl md:text-6xl font-semibold text-text-color">
+        Gas
+      </h1>
 
       <div className="w-full flex flex-col md:grid md:grid-cols-2 gap-4">
         <div className=" p-4 w-full h-96 overflow-hidden relative hover:grayscale">
@@ -14,15 +16,45 @@ const GasTab = () => {
             className="transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110"
           />
         </div>
-        <div className=" p-4 w-full h-96 flex flex-col items-center justify-center">
-          <p className="text-xl md:text-2xl text-black text-center">
-            Slitting, leveling and tension leveling, shearing and slearing,
-            plate burning, annealing, turning and polishing, warehousing and
-            repackaging, chemical milling, grinding, plasma cutting, saw
-            cutting, heat treating, honing, galvanizing, laser cutting,
-            centerless grinding, export packaging, production shearing and coil
-            leveling.
+        <div className=" p-4 w-full h-96 overflow-hidden relative hover:grayscale">
+          <Image
+            fill
+            src="/images/grind.jpg"
+            alt="img"
+            className="transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110"
+          />
+        </div>
+      </div>
+
+      <div className="w-full flex flex-col items-center justify-center gap-4">
+        <div className=" p-4 w-full h-96 flex flex-col items-center justify-center md:px-64">
+          <p className="text-xs md:text-sm text-text-color text-center">
+            We are also a reliable supplier of gas accross the whole country. We
+            are also a reliable supplier of gas accross the whole country. We
+            are also a reliable supplier of gas accross the whole country. We
+            are also a reliable supplier of gas accross the whole country. We
+            are also a reliable supplier of gas accross the whole country. We
+            are also a reliable supplier of gas accross the whole country. We
+            are also a reliable supplier of gas accross the whole country.
           </p>
+        </div>
+      </div>
+      <div className="w-full flex flex-col md:grid md:grid-cols-2 gap-4">
+        <div className=" p-4 w-full h-96 overflow-hidden relative hover:grayscale">
+          <Image
+            fill
+            src="/images/grind.jpg"
+            alt="img"
+            className="transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110"
+          />
+        </div>
+        <div className=" p-4 w-full h-96 overflow-hidden relative hover:grayscale">
+          <Image
+            fill
+            src="/images/grind.jpg"
+            alt="img"
+            className="transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110"
+          />
         </div>
       </div>
     </div>
--- components/pageComponets/services/enerygy/solarTab.tsx
+++ components/pageComponets/services/enerygy/solarTab.tsx
@@ -5,7 +5,7 @@ const SolarTab = () => {
   return (
     <div className="w-full h-full flex flex-col items-center justify-center gap-8">
       <h1 className="text-3xl md:text-6xl font-semibold text-text-color">
-        Solar
+        Solar Services
       </h1>
       <div className="w-full flex-col md:grid md:grid-cols-3 gap-4">
         <SolarCard image="/images/sol1.jpg" heading="Technical Services" />
